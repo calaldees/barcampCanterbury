c428f8067468d6366f60cf2cf2ad32bf2e1b6c01
Allan Callaghan <calaldees@gmail.com>
2019-03-16 22:25:54 +0000

    Started bootstrap for website Data for supports now in js files Makefiles to download boostrap dependencys Makefile to serve content

---
+++ barcampcanterbury.com/supporters.js
@@ -0,0 +1,28 @@
+build_supporters({
+	"2014": [
+		{href:"http://canthack.org/", name:"Martin Ellis", description:"Wondering Geek"},
+		{href:"http://calaldees.dreamhosters.com/", name:"Allan Callaghan", description:"@calaldees"},
+		{href:"http://theunixzoo.co.uk/", name:"Edd Barrett", description:"CompSci-Python-OpenBSD-Retro"},
+		{href:"https://www.youtube.com/watch?v:kfVsfOSbJY0", name:"Matt Hoskins", description:"Coles de Bruselas!"},
+		{href:"http://canthack.org/", name:"Tris Linnell", description:"#canthacker"},
+		{href:"http://canthack.org/", name:"Hannah Greer", description:"Geekery, employability, cats"},
+		{name:"Kieran Patel"},
+		{href:"https://atechmedia.com/careers", name:"Dan Wentworth", description:"Love Ruby? Come work with me"},
+		{href:"http://rl0.co.uk/", name:"Richard Lancaster"},
+		{href:"http://www.itscooper.co.uk/", name:"Christopher Cooper", description:"Security fanatic"},
+		{href:"http://perlfu.co.uk/", name:"Carl Ritson", description:"Mad Scientist"},
+		{href:"http://graymalk.in/", name:"Simon Cooksey"},
+		{name:"David Hobson", description:"Python, C# bespoke software dev!"},
+	],
+	"2013": [
+		{href:"http://canthack.org/", name:"Martin Ellis", description:"Wondering Geek"},
+		{href:"http://canthack.org/", name:"Tris Linnell", description:"#canthacker"},
+		{href:"http://canthack.org/", name:"Hannah Greer", description:"Geekery, employability, cats."},
+		{href:"http://soma.fm/", name:"Edd Barrett"},
+		{href:"http://canthack.org/", name:"Marianne Everett"},
+		{href:"https://www.youtube.com/watch?v:dQw4w9WgXcQ", name:"Robert Bronsdon"},
+		{href:"http://www.itscooper.co.uk/", name:"Christopher Cooper", description:"Security fanatic"},
+		{href:"http://canthack.org/", name:"Omer Kilic"},
+		{href:"http://www.g8osn.net/", name:"Brian Reay"},
+	],
+});
