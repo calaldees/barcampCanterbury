55c278a61d477e7dde9dd07208c4695523acb32d
Allan Callaghan <calaldees@gmail.com>
2019-03-19 14:51:34 +0000

    Progress with website added calendar link sponsors and supporters kind of using bootstrap cards

--- barcampcanterbury.com/sponsors.js
+++ barcampcanterbury.com/sponsors.js
@@ -1,3 +1,4 @@
 build_sponsors([
-    {name: 'test', href: 'test'},
+    {name: 'University of Kent: School of Computing', href: 'https://www.cs.kent.ac.uk/', href_text: 'Website',  description: 'The school of Computing has generously donated the venue Woolf College for this event'},
+    //image: 'https://www.cs.kent.ac.uk/webimages/home/about-us.png',
 ]);
