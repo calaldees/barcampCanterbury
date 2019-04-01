d9c1165dd494bb65baa16bcdd9c39180a3548666
Allan Callaghan <calaldees@gmail.com>
2019-04-01 12:18:39 +0100

    Added SVG logo Added SVG social icons

--- barcampcanterbury.com/sponsors.js
+++
@@ -1,4 +0,0 @@
-build_sponsors([
-    {name: 'University of Kent: School of Computing', href: 'https://www.cs.kent.ac.uk/', href_text: 'Website',  description: 'The school of Computing has generously donated the venue Woolf College for this event'},
-    //image: 'https://www.cs.kent.ac.uk/webimages/home/about-us.png',
-]);
