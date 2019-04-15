4abe30163b82f4fe1f03a21bd95c8c660d9924ee
Allan Callaghan <calaldees@gmail.com>
2019-04-15 13:46:24 +0100

    Added fruitworks as sponsor

--- barcampcanterbury.com/data/sponsors.js
+++ barcampcanterbury.com/data/sponsors.js
@@ -7,4 +7,12 @@ build_sponsors([
         image: 'https://www.cs.kent.ac.uk/webimages/home/about-us.png',
         twitter: 'UniKentComp',
     },
+    {
+        name: 'Fruitworks Coworking',
+        href: 'https://fruitworks.co/',
+        href_text: 'Website',
+        description: 'Fruitworks is a coworking space 30 seconds from Canterbury City Centre',
+        image: 'images/logo_fruitworksco.svg',
+        twitter: 'fruitworksco',
+    },
 ]);
