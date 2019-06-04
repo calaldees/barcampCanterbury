9a19434e3062fa7669fcbc70dfbf7ca69167c87a
Edd Barrett <vext01@gmail.com>
2019-06-04 22:15:04 +0100

    Add Deeson as a sponsor.

--- barcampcanterbury.com/data/sponsors.js
+++ barcampcanterbury.com/data/sponsors.js
@@ -24,4 +24,12 @@ build_sponsors([
         image: 'images/logo_lug.png',
         twitter: 'LevelUpGames2',
     },
+    {
+        name: 'Deeson',
+        href: 'https://www.deeson.co.uk/',
+        href_text: 'Website',
+        description: 'Canterbury-based digital agency Deeson designs and builds web platforms for some of the biggest brands in the UK.<br /><br />Deeson are <a href="https://www.deeson.co.uk/careers">currently hiring</a>.',
+        image: 'images/deeson-logo.png',
+        twitter: 'deesonagency',
+    },
 ]);
